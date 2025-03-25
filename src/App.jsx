fdef7065cd03e024e49dea0963bf9f377b3c033d
JanLascala <lascala.jan@gmail.com>
2025-03-25 16:46:21 +0100

    add language selection feature and style updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 // Import data
 import languages from "./data/languages";
 
 function App() {
+    const [selectedLanguage, setSelectedLanguage] = useState(languages[0]);
 
     return (
-        
-    )
-}
+        <>
+            <h1 className="title">Learn web development</h1>
+            <div>
+                {languages.map((language) => (
+                    <button className="btn" key={language.id} onClick={() => setSelectedLanguage(language)}>
+                        {language.name}
+                    </button>
+                ))}
+                <div className="container">
+                    <h2>{selectedLanguage.title}</h2>
+                    <p className="paragraph">{selectedLanguage.description}</p>
+                </div>
 
-export default App;
+            </div>
+        </>
+    )
+};
+
+export default App
