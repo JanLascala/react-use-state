ca1e712a16c888effa8348d1bec6395a6c887b6b
JanLascala <lascala.jan@gmail.com>
2025-03-25 15:42:28 +0100

    create array import to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+// Import data
+import languages from "./data/languages";
 
 function App() {
 
+    return (
+        
+    )
 }
 
-export default App
+export default App;
---
+++ src/data/languages.js
@@ -0,0 +1,40 @@
+const languages = [
+    {
+        id: 1,
+        title: "HTML",
+        name: "HTML",
+        description: "The standard markup language for creating web pages."
+    },
+    {
+        id: 2,
+        title: "CSS",
+        name: "CSS",
+        description: "A stylesheet language used to style the appearance of web pages."
+    },
+    {
+        id: 3,
+        title: "JavaScript",
+        name: "JavaScript",
+        description: "A versatile programming language for creating interactive web experiences."
+    },
+    {
+        id: 4,
+        title: "Node.js",
+        name: "Node.js",
+        description: "A runtime environment that allows JavaScript to run on the server side."
+    },
+    {
+        id: 5,
+        title: "Express",
+        name: "Express",
+        description: "A minimal and flexible Node.js framework for building web applications and APIs."
+    },
+    {
+        id: 6,
+        title: "React.js",
+        name: "React.js",
+        description: "A JavaScript library for building user interfaces, maintained by Facebook."
+    }
+];
+
+export default languages;
